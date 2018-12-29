3c9cd0a1cebc8489f65436dd3bb55f8058550b0a
subhankar <roysubho687@gmail.com>
2018-12-30 03:23:55 +0530

    v0.3 initial baseline

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Persons = (props) => {
+    const styles = {
+        border: '1px solid #ccc',
+        margin: '8px 0 8px 4px',
+        padding: '4px 0 10px 4px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        boxShadow: '0 5px 5px darkblue'
+    }
     return (
-        <div>
+        <div style={styles}>
             <p>{props.name} is of {props.age}</p>
             <button type="button" onClick={props.clickFromChild}>click in child change in parent</button>
+            <button type="button" onClick={props.clickFromChildDelete}>Delete Person</button>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed}/>
         </div>
--- src/components/state-management/App3.js
+++ src/components/state-management/App3.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import Persons from '../Persons/Persons';
 class App3 extends Component {
+    // constructor () {
+    //     super();
+    //     // declare global variable for this component here react new feature in 16.7.0 release
+    // }
     state = {
         persons: [
-            {name: 'Subhankar', age: '25'},
-            {name: 'Vidyotma', age: '25'},
-            {name: 'Goutam', age: '29'}
-        ]
+            {id: 1, name: 'Subhankar', age: '25'},
+            {id: 2, name: 'Vidyotma', age: '25'},
+            {id: 3, name: 'Goutam', age: '29'}
+        ],
+        showPersons: false
     }
     /**
      * A normal ES6 funtion to chnage the stateS
@@ -37,22 +42,62 @@ class App3 extends Component {
     twoWayCheck = (event) => {
         console.log(event.target.value);
     }
+    showPersonsList = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
+    deletePerson = (personIndex) => {
+        // const persons = this.state.persons; // DO NOT do this directly accessing state variable will mutate state
+        // create a copy of state variable and use 
+        // method one 
+        // const persons = this.state.persons.slice();
+        // method 2 spread operator and recommended
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    }
+    myLoop() {
+        // note map function if in jsx callback does not need a braces else in js it needs
+        this.state.persons.map((fn, i) => {
+            return console.log(fn);
+        });
+    }
+    /**
+     * React Life cycle hook
+     */
+    componentDidMount = () => {
+        this.myLoop();
+    }
     render() {
         return (
             <div id="bg1">
                 <button onClick={this.changeStateHandler.bind(this,'thug')}>Change State Single</button>
                 <button onClick={this.changeStateHandlerAll}>Change State All</button>
-                <Persons 
-                    name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age}/>
-                <Persons 
-                    name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age}
-                    clickFromChild={this.changeStateHandlerAll}
-                    changed={this.twoWayCheck}>Try this for click from child to change parent state</Persons>
-                <Persons 
-                    name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age}/>
+                <button onClick={this.showPersonsList}>Toggle Persons</button>
+                {this.state.showPersons ? 
+                    <div>
+                        <Persons 
+                            name={this.state.persons[0].name} 
+                            age={this.state.persons[0].age}/>
+                        <Persons 
+                            name={this.state.persons[1].name} 
+                            age={this.state.persons[1].age}
+                            clickFromChild={this.changeStateHandlerAll}
+                            changed={this.twoWayCheck}>Try this for click from child to change parent state</Persons>
+                        <Persons 
+                            name={this.state.persons[2].name} 
+                            age={this.state.persons[2].age}/>
+                    </div> : null   
+                }
+                <hr/>
+                <p>Persons through list</p>
+                {this.state.persons.map((person, i) => 
+                    <Persons name={person.name} 
+                             age={person.age} 
+                             clickFromChildDelete={this.deletePerson.bind(this, i)}
+                             key={person.id}/>
+                )}
+                <p>Persons through js function</p>
             </div>
         )
     }
